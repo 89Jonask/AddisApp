66b51166e7381dd8170b103d3ce8718ce3f87657
89Jonask <72088076+89Jonask@users.noreply.github.com>
2020-10-25 10:15:05 +0100

    created react project and started to build

---
+++ src/components/collectioncontainer/CollectionConatiner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CollectionContainer.css";
+
+function CollectionContainer() {
+  return (
+    <div class="CollectionContainer">
+      <row>
+        <img src="./public/logo192.png"></img>
+      </row>
+      <row>
+        <img src="./public/logo192.png"></img>
+      </row>
+    </div>
+  );
+}
+
+export default CollectionContainer;
---
+++ src/components/navigationbar/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavigationBar.css";
+
+function NavigationBar() {
+  return (
+    <div class="topnav" id="myTopnav">
+      <a class="topnavpage" href="#home">
+        Home
+      </a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="/Users/alidabard/Alida/About/about.html">About</a>
+    </div>
+  );
+}
+
+export default NavigationBar;
